78afbc6efe9c112f461b43418e567472acb732b2
Troy Ankersen <caskale@outlook.com>
2022-04-13 11:56:25 -0500

    inventory updates

--- ansible-user-setup.sh
+++ ansible-user-setup.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
 
-HOSTS='mordecai'
+HOSTS="arch"
+USER="troy"
 
 echo "Creating ansible user on ${HOSTS} group."
 echo ""
 
 # ensure ansible user exists
 ansible ${HOSTS} \
+  --user ${USER} \
   --module-name user \
   --args "name=ansible \
           state=present" \
   --become \
-  --ask-become-pass
-    
+  --ask-become-pass \
+  --ask-pass
+
 # add authorized key for ansible user
 ansible ${HOSTS} \
+  --user ${USER} \
   --module-name authorized_key \
   --args "key={{ lookup('file', '/home/troy/data/keys/ssh/id_ed25519-ansible.pub') }} \
           user=ansible state=present" \
   --become \
-  --ask-become-pass
- 
+  --ask-become-pass \
+  --ask-pass
+
 # ensure sudoers allowance for group ansible
 ansible ${HOSTS} \
+  --user ${USER} \
   --module-name copy \
   --args "mode='0440' \
           content='%ansible ALL=(ALL:ALL) NOPASSWD: ALL\n' \
           dest='/etc/sudoers.d/ansible'" \
   --become \
-  --ask-become-pass
+  --ask-become-pass \
+  --ask-pass
